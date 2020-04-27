53d338d26cc91a75a91bb111317badd252c67fec
mrudula.shridhara <mrudula.shridhara@SVINDK00093.SPI.COM>
2020-04-27 17:57:24 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a=20, b=30, sum;
+  
+		 sum=a+b;
+		 System.out.println("The sum is " +sum);
+		 System.out.println("Hello World");
+		 
+	}
+
+}
